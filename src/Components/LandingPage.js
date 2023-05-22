a61fd262d27f19be14ba56b0aaadac4330377da1
Anushka <bhardwajanushka180@gmail.com>
2023-05-22 22:30:53 +0530

    removed heading and para div from Landingpage

--- src/Components/LandingPage.js
+++ src/Components/LandingPage.js
@@ -4,7 +4,7 @@ import { FaSearch } from "react-icons/fa";
 const LandingPage = () => {
   return (
     <>
-      <div className="bg-cBackgroundBlue min-h-screen w-full flex flex-col relative">
+      <div className="bg-cBackgroundBlue min-h-screen w-full flex relative">
         <img
           className="w-48 rounded-3xl absolute inset-x-1/3 top-1"
           src="https://images.unsplash.com/photo-1565299624946-b28f40a0ae38?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8Nnx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=400&q=60"
@@ -30,14 +30,6 @@ const LandingPage = () => {
         <div className="absolute inset-y-1/2 left-64 w-52 h-20 rounded-full flex justify-center items-center bg-white cursor-pointer">
           <FaSearch className="text-cSearchIcon pt-1 pr-3 text-3xl font-extrabold" />
           <h1 className="text-cSearchText font-bold text-xl">hey! foodie</h1>
-        </div>
-
-        <div className="flex justify-center items-center">
-          <h1>Search for an idea</h1>
-          <p>
-            What do you want to try next? Think of something you’re into—like
-            “easy chicken dinner”—and see what you find.
-          </p>
         </div>
       </div>
     </>
