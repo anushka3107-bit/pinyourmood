50b3094121f8a8df2e5414e369e260d6f612d59b
Nishant Chauhan <nishantpssn720@gmail.com>
2023-05-24 01:23:38 +0530

    signup page created

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import LandingPage from "./Components/LandingPage";
 import Sidebar from "./Components/Sidebar";
-import Navbar from "./Components/Navbar"
-import { Routes, Route, BrowserRouter } from "react-router-dom";
+import Navbar from "./Components/Navbar";
+import SignUp from "./Components/SignUpPage";
 import LandingPageThirdPage from "./Components/LandingPageThirdPage";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+
 
 function App() {
   return (
@@ -11,8 +13,9 @@ function App() {
         <Routes>
           <Route path="/" element={<Navbar />} />
           <Route path="/sidebar" element={<Sidebar />} />
+          <Route path="/signup" element={<SignUp />} />
           <Route path="/landingpage" element={<LandingPage />} />
-          <Route path="/landingpagethird" element={<LandingPageThirdPage/>} />
+          <Route path="/landingpagethird" element={<LandingPageThirdPage />} />
         </Routes>
       </BrowserRouter>
     </div>
---
+++ src/Components/SignUpPage.js
@@ -0,0 +1,54 @@
+import React from "react";
+
+const SignUpPage = () => {
+  return (
+    <div className="">
+      <img
+        src="https://i.pinimg.com/550x/ab/d5/0b/abd50b8d71a8ac7eada5ae632ebfcf90.jpg"
+        alt=""
+      ></img>
+      <img
+        src="https://i.pinimg.com/550x/97/75/c9/9775c94453909dedc08cb48b0e4cbb65.jpg"
+        alt=""
+      ></img>
+      <img
+        src="https://i.pinimg.com/550x/43/01/50/43015082f34d62db3fac51f9a3bb6a49.jpg"
+        alt=""
+      ></img>
+      <img
+        src="https://i.pinimg.com/550x/88/89/77/888977ac13d1227b51ec48f8ebc29b54.jpg"
+        alt=""
+      ></img>
+      <img
+        src="https://i.pinimg.com/550x/b4/cb/74/b4cb743c46103d2502b48f05308bfae5.jpg"
+        alt=""
+      ></img>
+      <img
+        src="https://i.pinimg.com/550x/1e/48/7b/1e487b8c1d22a689b45e6f495b3b856b.jpg"
+        alt=""
+      ></img>
+      <img
+        src="https://i.pinimg.com/550x/bc/c0/09/bcc009e050c7d297cfd036466d41ee4d.jpg"
+        alt=""
+      ></img>
+      <img
+        src="https://i.pinimg.com/550x/d8/f0/b9/d8f0b93c4cac03a9149b08afea4d6ba2.jpg"
+        alt=""
+      ></img>
+      <img
+        src="https://i.pinimg.com/550x/19/02/7b/19027b0692b82ea30598e76f0633ebc0.jpg"
+        alt=""
+      ></img>
+      <img
+        src="https://i.pinimg.com/550x/48/65/77/48657791d2e6d1400e882250f44d3d0e.jpg"
+        alt=""
+      ></img>
+      <img
+        src="https://i.pinimg.com/550x/ee/c6/34/eec634f370bdfec4bf4bf29d6c2969d2.jpg"
+        alt=""
+      ></img>
+    </div>
+  );
+};
+
+export default SignUpPage;
