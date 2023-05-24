9233014584974c7517109076436ed92de4a4512a
Nishant Chauhan <nishantpssn720@gmail.com>
2023-05-24 16:47:44 +0530

    aadha hua h signup page

--- src/Components/SignUpPage.js
+++ src/Components/SignUpPage.js
@@ -2,51 +2,160 @@ import React from "react";
 
 const SignUpPage = () => {
   return (
-    <div className="">
-      <img
-        src="https://i.pinimg.com/550x/ab/d5/0b/abd50b8d71a8ac7eada5ae632ebfcf90.jpg"
-        alt=""
-      ></img>
-      <img
-        src="https://i.pinimg.com/550x/97/75/c9/9775c94453909dedc08cb48b0e4cbb65.jpg"
-        alt=""
-      ></img>
-      <img
-        src="https://i.pinimg.com/550x/43/01/50/43015082f34d62db3fac51f9a3bb6a49.jpg"
-        alt=""
-      ></img>
-      <img
-        src="https://i.pinimg.com/550x/88/89/77/888977ac13d1227b51ec48f8ebc29b54.jpg"
-        alt=""
-      ></img>
-      <img
-        src="https://i.pinimg.com/550x/b4/cb/74/b4cb743c46103d2502b48f05308bfae5.jpg"
-        alt=""
-      ></img>
-      <img
-        src="https://i.pinimg.com/550x/1e/48/7b/1e487b8c1d22a689b45e6f495b3b856b.jpg"
-        alt=""
-      ></img>
-      <img
-        src="https://i.pinimg.com/550x/bc/c0/09/bcc009e050c7d297cfd036466d41ee4d.jpg"
-        alt=""
-      ></img>
-      <img
-        src="https://i.pinimg.com/550x/d8/f0/b9/d8f0b93c4cac03a9149b08afea4d6ba2.jpg"
-        alt=""
-      ></img>
-      <img
-        src="https://i.pinimg.com/550x/19/02/7b/19027b0692b82ea30598e76f0633ebc0.jpg"
-        alt=""
-      ></img>
-      <img
-        src="https://i.pinimg.com/550x/48/65/77/48657791d2e6d1400e882250f44d3d0e.jpg"
-        alt=""
-      ></img>
-      <img
-        src="https://i.pinimg.com/550x/ee/c6/34/eec634f370bdfec4bf4bf29d6c2969d2.jpg"
-        alt=""
-      ></img>
+    <div className="flex flex-row space-x-5 overflow-hidden">
+      {/* 1st */}
+      <div className="space-y-5 -ml-16">
+        <div className="h-80 w-60">
+          <img
+            className="rounded-2xl object-cover mt-5 h-full w-full"
+            src="https://i.pinimg.com/550x/0c/18/3d/0c183dafd58d1ebdbe3cb6865e56d516.jpg"
+            alt=""
+          ></img>
+        </div>
+        <div className="h-80 w-60">
+          <img
+            className="rounded-2xl object-cover h-full w-full"
+            src="https://i.pinimg.com/550x/1d/ce/e7/1dcee7977b922d38bf7873462793aec5.jpg"
+            alt=""
+          ></img>
+        </div>
+      </div>
+
+      {/* 2nd */}
+      <div className="space-y-5 -mt-16">
+        <div className="h-80 w-60">
+          <img
+            className="rounded-2xl object-cover h-full w-full"
+            src="https://i.pinimg.com/550x/0e/6e/0f/0e6e0f35b79799c6d18f47e54ebb28f7.jpg"
+            alt=""
+          ></img>
+        </div>
+        <div className="h-80 w-60">
+          <img
+            className="rounded-2xl object-cover h-full w-full"
+            src="https://i.pinimg.com/550x/ab/36/4c/ab364c9df3ce6ff3e1e5834c8359f2da.jpg"
+            alt=""
+          ></img>
+        </div>
+        <div className="h-80 w-60">
+          <img
+            className="rounded-2xl object-cover h-full w-full"
+            src="https://i.pinimg.com/550x/bc/c0/09/bcc009e050c7d297cfd036466d41ee4d.jpg"
+            alt=""
+          ></img>
+        </div>
+      </div>
+
+      {/* 3rd */}
+      <div className="space-y-5 -mt-20">
+        <div className="h-80 w-60">
+          <img
+            className="rounded-2xl object-cover h-full w-full"
+            src="https://i.pinimg.com/550x/1e/48/7b/1e487b8c1d22a689b45e6f495b3b856b.jpg"
+            alt=""
+          ></img>
+        </div>
+        <div className="h-80 w-60">
+          <img
+            className="rounded-2xl object-cover h-full w-full"
+            src="https://i.pinimg.com/550x/a9/de/84/a9de841ad3df74fa18a238a5eaeade67.jpg"
+            alt=""
+          ></img>
+        </div>
+        <div className="h-80 w-60">
+          <img
+            className="rounded-2xl object-cover h-full w-full"
+            src="https://i.pinimg.com/550x/4c/a7/40/4ca740cb0b6e36c207a15538942474c2.jpg"
+            alt=""
+          ></img>
+        </div>
+      </div>
+
+      {/* 4th */}
+      <div className="space-y-5">
+        <div className="h-80 w-60">
+          <img
+            className="rounded-2xl object-cover h-full w-full"
+            src="https://i.pinimg.com/550x/9c/9a/e6/9c9ae6cbed094e34a7cc75fd1e955c50.jpg"
+            alt=""
+          ></img>
+        </div>
+        <div className="h-90 w-60">
+          <img
+            className="rounded-2xl object-cover h-full w-full"
+            src="https://i.pinimg.com/550x/3b/d5/ef/3bd5ef3dc79ad219f69b21ca6c376273.jpg"
+            alt=""
+          ></img>
+        </div>
+      </div>
+
+      {/* 5th */}
+      <div className="space-y-5">
+        <div className="h-80 w-60">
+          <img
+            className="rounded-2xl object-cover h-full w-full"
+            src="https://i.pinimg.com/550x/7e/b3/bd/7eb3bdec5120c9b5bb5ae06e1bf92866.jpg"
+            alt=""
+          ></img>
+        </div>
+        <div className="h-80 w-60">
+          <img
+            className="rounded-2xl object-cover h-full w-full"
+            src="https://i.pinimg.com/550x/8b/4d/12/8b4d12c450bf458f6ee958bb2a980bce.jpg"
+            alt=""
+          ></img>
+        </div>
+        <div className="h-80 w-60">
+          <img
+            className="rounded-2xl object-cover h-full w-full"
+            src="https://i.pinimg.com/550x/20/8f/ba/208fba35fdb6153218c014f6af21e908.jpg"
+            alt=""
+          ></img>
+        </div>
+      </div>
+
+      {/* 6th */}
+      <div className="space-y-5">
+        <div className="h-80 w-60">
+          <img
+            className="rounded-2xl object-cover h-full w-full"
+            src="https://i.pinimg.com/550x/34/01/2b/34012bb7951515ef5c1c640ccd5a1861.jpg"
+            alt=""
+          ></img>
+        </div>
+        <div className="h-80 w-60">
+          <img
+            className="rounded-2xl object-cover h-full w-full"
+            src="https://i.pinimg.com/550x/9c/9a/e6/9c9ae6cbed094e34a7cc75fd1e955c50.jpg"
+            alt=""
+          ></img>
+        </div>
+        <div className="h-80 w-60">
+          <img
+            className="rounded-2xl object-cover h-full w-full"
+            src="https://i.pinimg.com/550x/ef/9e/d4/ef9ed4bebeefde520637d901774ad439.jpg"
+            alt=""
+          ></img>
+        </div>
+      </div>
+
+      {/* 7th */}
+      <div className="space-y-5">
+        <div className="h-80 w-60">
+          <img
+            className="rounded-2xl mt-5 object-cover h-full w-full"
+            src="https://i.pinimg.com/550x/ab/d5/0b/abd50b8d71a8ac7eada5ae632ebfcf90.jpg"
+            alt=""
+          ></img>
+        </div>
+        <div className="h-80 w-60">
+          <img
+            className="rounded-2xl object-cover h-full w-full"
+            src="https://i.pinimg.com/550x/48/65/77/48657791d2e6d1400e882250f44d3d0e.jpg"
+            alt=""
+          ></img>
+        </div>
+      </div>
     </div>
   );
 };
