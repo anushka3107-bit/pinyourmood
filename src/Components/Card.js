adeea0fe35b4b7cd0d1aba665e1cd22667f6af4d
Anushka <bhardwajanushka180@gmail.com>
2023-05-25 22:26:44 +0530

    creating filter component

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -3,16 +3,13 @@ import { filterdata } from "../data/filterdata";
 
 const Card = ({ items }) => {
   return (
-    <div className="flex">
+    <div className="flex ">
       <h1>WERTYUIO</h1>
-      {items?.map((item) => (
-        <img
-          src={item.imgURL}
-          alt={item.title}
-          key={item.id}
-          className="bg-pink-300 h-20 w-20 t-20"
-        />
-      ))}
+      <div className="bg-pink-300 h-20 w-20 t-20">
+        {items?.map((item) => (
+          <img src={item.imgURL} alt={item.title} key={item.id} />
+        ))}
+      </div>
     </div>
   );
 };
