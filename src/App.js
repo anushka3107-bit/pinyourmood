bb33cec1657781e11ad3342dc7cbebe9741593f5
Anushka <bhardwajanushka180@gmail.com>
2023-05-25 18:34:48 +0530

    added main page as the landing page

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import LandingPage from "./Components/LandingPage";
 import Sidebar from "./Components/Sidebar";
-import Navbar from "./Components/Navbar";
 import SignUp from "./Components/SignUpPage";
 import LandingPageThirdPage from "./Components/LandingPageThirdPage";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
+import LandingPageTop from "./Components/LandingPageTop";
 
 function App() {
   return (
     <div className="flex">
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Navbar />} />
+          <Route path="/" element={<LandingPageTop />} />
           <Route path="/sidebar" element={<Sidebar />} />
           <Route path="/signup" element={<SignUp />} />
           <Route path="/landingpage" element={<LandingPage />} />
---
+++ src/Components/LandingPageTop.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Navbar from "./Navbar";
+import LandingPageBottom from "./LandingPageBottom";
+
+const LandingPageTop = () => {
+  return (
+    <>
+      <div className="w-full container-snap overflow-x-auto">
+        <div>
+          <Navbar className="w-full" />
+        </div>
+        <div className="flex h-screen text-center justify-center">
+          <h1 className="text-6xl pt-32 font-semibold">Get Your Next</h1>
+        </div>
+
+        <LandingPageBottom />
+      </div>
+    </>
+  );
+};
+
+export default LandingPageTop;
