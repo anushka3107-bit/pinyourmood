bdf05ad021abcd259b4516d434cce01a3ca8c7d2
Anushka <bhardwajanushka180@gmail.com>
2023-06-01 16:11:39 +0530

    fixed search bar

--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -1,48 +1,57 @@
 import React from "react";
 import { BiSearchAlt } from "react-icons/bi";
 import { BsBellFill } from "react-icons/bs";
-import { AiFillMessage } from "react-icons/ai";
+import { FaCommentDots } from "react-icons/fa";
 import { FaUserCircle } from "react-icons/fa";
-import { IoIosArrowDropdown } from "react-icons/io";
+import { FiChevronDown } from "react-icons/fi";
 const SearchBar = () => {
   return (
     <>
-      <div className="flex relative w-full shadow-[0px_5px_20px_rgba(0,0,0,0.15)] mt-0 px-0 py-2.5">
-        <div className="w-[50px] min-w-[50px] m-[5px]">
+      <div className="flex justify-between w-full h-20 shadow-[0px_5px_20px_rgba(0,0,0,0.15)]">
+        <div className="flex items-center justify-center pl-10 cursor-pointer">
           <img
-            src="https://www.freepnglogos.com/uploads/pinterest-logo-p-png-0.png"
-            alt=""
+            className="w-10 h-10"
+            src="https://i.pinimg.com/236x/8e/e1/23/8ee12366506cd70d2896ea0543934e3e.jpg"
+            alt="logo"
           />
+
+          <div className="pl-10 text-lg font-semibold">
+            <button className="px-4 py-2 rounded-3xl text-gray-600 hover:text-white hover:bg-cHoverColorOrange">
+              Home
+            </button>
+          </div>
+
+          <div className=" pl-5 text-lg font-semibold">
+            <button className="px-4 py-2 rounded-3xl text-gray-600 hover:text-white hover:bg-cHoverColorOrange ">
+              Explore
+            </button>
+          </div>
         </div>
-        <button className="w-[70px] m-2 rounded-[50px] hover:text-[white] hover:transition-[0.3s] hover:bg-black">
-          Home
-        </button>
-        <button className="w-[70px] m-2 rounded-[50px] hover:text-[white] hover:transition-[0.3s] hover:bg-black ">
-          Explore
-        </button>
-        <div className="flex w-[70%] cursor-pointer h-10 bg-[#e9e9e9] mt-5 m-2 px-[15px] py-2 rounded-[50px] border-2 border-solid border-[rgba(0,0,0,0.1)]">
+
+        <div className="flex w-3/5 cursor-pointer h-10 bg-[#e9e9e9] mt-5 m-2 px-[15px] py-2 rounded-[50px] ">
           <input
-            className="w-[75rem] text-black bg-transparent border-r-2 border-r-[#0000001a] border-solid justify-left"
+            className="w-[75rem] text-gray-500 bg-transparent border-r-2 border-r-gray-300  border-solid justify-left outline-none border-none"
             type="text"
             placeholder="Search Interests"
           />
-          <div className="flex justify-center w-[10%] mr-0">
-            <BiSearchAlt />
+          <div className="flex justify-center items-center w-[10%] ">
+            <BiSearchAlt className="text-2xl text-gray-500" />
           </div>
         </div>
-        <div className="w-[90px] flex text-xl cursor-pointer h-10 mt-2">
-          <button className="p-2 rounded-[50%] hover:bg-[#e9e9e9]">
+
+        <div className="flex text-2xl cursor-pointer items-center justify-end mr-5 ml-0 ">
+          <div className="text-gray-600 py-2 px-2  hover:bg-gray-200 rounded-full">
             <BsBellFill />
-          </button>
-          <button className="p-2 rounded-[50%] hover:bg-[#e9e9e9]">
-            <AiFillMessage />
-          </button>
-          <button className="p-2 rounded-[50%] hover:bg-[#e9e9e9]">
+          </div>
+          <div className="text-gray-600 py-2 px-2 hover:bg-gray-200 rounded-full">
+            <FaCommentDots />
+          </div>
+          <div className="text-gray-600 py-2 px-2 hover:bg-gray-200 rounded-full">
             <FaUserCircle />
-          </button>
-          <button className="p-2 rounded-[50%] hover:bg-[#e9e9e9]">
-            <IoIosArrowDropdown />
-          </button>
+          </div>
+          <div className="text-gray-600  py-2 px-2 hover:bg-gray-200 rounded-full">
+            <FiChevronDown />
+          </div>
         </div>
       </div>
     </>
