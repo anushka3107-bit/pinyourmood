be173f36ab763187f51e97a94dfa572edb3735c8
Anushka <bhardwajanushka180@gmail.com>
2023-06-03 00:10:03 +0530

    updated hover color

--- src/Components/FooterHimanshu.js
+++ src/Components/FooterHimanshu.js
@@ -5,7 +5,7 @@ const FooterHimanshu = () => {
     <>
       <div className="flex w-full bg-gray-200 items-center h-screen justify-between ">
         <div className="flex flex-col justify-center items-center w-1/2 pl-8">
-          <h4 className="text-3xl font-bold hover:text-white">About Us</h4>
+          <h4 className="text-3xl font-bold hover:text-red-500">About Us</h4>
 
           <div className="flex flex-col justify-center items-center p-4">
             <p className="font-semibold">
@@ -20,32 +20,32 @@ const FooterHimanshu = () => {
 
           <div className="flex cursor-pointer space-evenly">
             <a href="/terms">
-              <span className="hover:text-white"> Terms & Conditions </span>
+              <span className="hover:text-red-500"> Terms & Conditions </span>
             </a>
             <a href="/privacy">
-              <span className="px-4 hover:text-white"> Privacy Policy</span>
+              <span className="px-4 hover:text-red-500"> Privacy Policy</span>
             </a>
           </div>
 
-          <div className="flex flex-col cursor-pointer hover:text-white">
+          <div className="flex flex-col cursor-pointer hover:text-red-500">
             <span>@Copyright {new Date().getFullYear()}.</span>
           </div>
         </div>
 
         <div className="flex flex-col justify-end items-end w-1/2 pr-8 cursor-pointer">
           <div className="flex flex-col justify-center items-center">
-            <h4 className="font-bold text-xl hover:text-white">Team Members</h4>
+            <h4 className="font-bold text-xl hover:text-red-500">Team Members</h4>
             <a href="/employer">
-              <span className="hover:text-white">Anushka Bhardwaj</span>
+              <span className="hover:text-red-500">Anushka Bhardwaj</span>
             </a>
             <a href="/employer">
-              <span className="hover:text-white">Nishant Chauhan</span>
+              <span className="hover:text-red-500">Nishant Chauhan</span>
             </a>
             <a href="/employer">
-              <span className="hover:text-white">Himanshu Bhatt</span>
+              <span className="hover:text-red-500">Himanshu Bhatt</span>
             </a>
             <a href="/employer">
-              <span className="hover:text-white">Unnati Bhole</span>
+              <span className="hover:text-red-500">Unnati Bhole</span>
             </a>
             <div className="pt-3">
               <button className="bg-orange-600 rounded-full px-3 py-3 hover:bg-orange-400 hover:text-white">
