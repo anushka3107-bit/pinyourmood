6f6340be6da5c058a5b182a2742a63290d032726
Anushka <bhardwajanushka180@gmail.com>
2023-06-13 22:51:32 +0530

    added images in main page

--- src/data/PinData.js
+++ src/data/PinData.js
@@ -2,242 +2,418 @@ const pindata = [
   {
     id: 1,
     imgSrc:
-      "https://cdn.pixabay.com/photo/2021/12/01/17/28/branch-6838719_960_720.jpg",
+      "https://i.pinimg.com/236x/6c/6d/d6/6c6dd6937ec835b6eb3182cb5dd0beee.jpg",
     name: "Vladvictoria",
-    link: "https://pixabay.com/photos/branch-ornaments-lights-tree-6838719/",
+    link: "https://i.pinimg.com/236x/6c/6d/d6/6c6dd6937ec835b6eb3182cb5dd0beee.jpg",
     size: "small",
   },
   {
     id: 2,
     imgSrc:
-      "https://cdn.pixabay.com/photo/2021/12/05/21/39/christmas-balls-6848782_960_720.jpg",
-    name: "Mammiya",
-    link: "https://pixabay.com/photos/christmas-balls-christmas-advent-6848782/",
+      "https://i.pinimg.com/236x/0d/f6/52/0df6521355751582b278bd8f7da2ca62.jpg",
+    name: "Vladvictoria",
+    link: "https://i.pinimg.com/236x/0d/f6/52/0df6521355751582b278bd8f7da2ca62.jpg",
     size: "medium",
   },
   {
     id: 3,
     imgSrc:
-      "https://cdn.pixabay.com/photo/2020/07/29/03/58/child-5446839_960_720.jpg",
-    name: "Zhangliams",
-    link: "https://pixabay.com/photos/child-boy-run-alley-street-plants-5446839/",
-    size: "large",
-  },
-  {
-    id: 4,
-    imgSrc:
-      "https://cdn.pixabay.com/photo/2016/11/14/09/14/cat-1822979_960_720.jpg",
-    name: "Mshyun",
-    link: "https://pixabay.com/photos/cat-christmas-tree-ornaments-1822979/",
-    size: "medium",
-  },
-  {
-    id: 5,
-    imgSrc:
-      "https://cdn.pixabay.com/photo/2021/11/02/10/46/lemur-6762935_960_720.jpg",
-    name: "Jansheldan",
-    link: "https://pixabay.com/photos/lemur-animal-wilderness-nature-6762935/",
-    size: "large",
-  },
-  {
-    id: 6,
-    imgSrc:
-      "https://cdn.pixabay.com/photo/2021/10/13/07/43/couple-6705694_960_720.jpg",
-    name: "Jupilu",
-    link: "https://pixabay.com/photos/couple-sundown-field-sunset-6705694/",
-    size: "small",
-  },
-  {
-    id: 7,
-    imgSrc:
-      "https://cdn.pixabay.com/photo/2017/02/08/17/24/fantasy-2049567_960_720.jpg",
-    name: "Stergo",
-    link: "https://pixabay.com/photos/fantasy-butterflies-mushrooms-2049567/",
-    size: "medium",
-  },
-  {
-    id: 8,
-    imgSrc:
-      "https://cdn.pixabay.com/photo/2015/11/16/16/28/bird-1045954_960_720.jpg",
-    name: "Homecare",
-    link: "https://pixabay.com/photos/bird-fluttering-berries-wings-tit-1045954/",
-    size: "large",
-  },
-  {
-    id: 9,
-    imgSrc:
-      "https://cdn.pixabay.com/photo/2021/11/26/20/44/lantern-6826687_960_720.jpg",
-    name: "Lasrs Nissen",
-    link: "https://pixabay.com/photos/lantern-sea-sky-beach-water-6826687/",
-    size: "medium",
-  },
-  {
-    id: 10,
-    imgSrc:
-      "https://cdn.pixabay.com/photo/2021/11/30/17/06/tree-6835828_960_720.jpg",
-    name: "Jell Wllington",
-    link: "https://pixabay.com/photos/tree-snow-winter-christmas-branch-6835828/",
-    size: "medium",
-  },
-  {
-    id: 11,
-    imgSrc:
-      "https://cdn.pixabay.com/photo/2012/06/19/10/32/owl-50267_960_720.jpg",
-    name: "Chraecker",
-    link: "https://pixabay.com/photos/owl-bird-animal-bird-of-prey-50267/",
-    size: "small",
-  },
-  {
-    id: 12,
-    imgSrc:
-      "https://cdn.pixabay.com/photo/2017/06/07/10/47/elephant-2380009_960_720.jpg",
-    name: "Comfreak",
-    link: "https://pixabay.com/photos/elephant-watering-hole-baby-elephant-2380009/",
-    size: "medium",
-  },
-  {
-    id: 13,
-    imgSrc:
-      "https://cdn.pixabay.com/photo/2016/11/08/05/26/woman-1807533_960_720.jpg",
-    name: "Sasinet",
-    link: "https://pixabay.com/photos/woman-kid-rain-leaf-umbrella-1807533/",
-    size: "large",
-  },
-  {
-    id: 14,
-    imgSrc:
-      "https://cdn.pixabay.com/photo/2019/08/19/07/45/dog-4415649_960_720.jpg",
-    name: "Hauad",
-    link: "https://pixabay.com/photos/dog-corgi-pets-cute-rain-4415649/",
-    size: "medium",
-  },
-  {
-    id: 15,
-    imgSrc:
-      "https://cdn.pixabay.com/photo/2017/01/28/02/24/japan-2014619_960_720.jpg",
-    name: "Chau min",
-    link: "https://pixabay.com/photos/japan-osaka-night-asia-landmark-2014619/",
-    size: "large",
-  },
-  {
-    id: 16,
-    imgSrc:
-      "https://cdn.pixabay.com/photo/2015/10/22/17/45/leaf-1001679_960_720.jpg",
-    name: "Steven",
-    link: "https://pixabay.com/photos/leaf-green-foliage-green-leaves-1001679/",
-    size: "medium",
-  },
-  {
-    id: 17,
-    imgSrc:
-      "https://cdn.pixabay.com/photo/2018/05/16/22/27/rose-3407234_960_720.jpg",
-    name: "Rosee",
-    link: "https://pixabay.com/photos/rose-red-drops-rain-macro-nature-3407234/",
-    size: "medium",
-  },
-  {
-    id: 18,
-    imgSrc:
-      "https://cdn.pixabay.com/photo/2018/01/29/19/00/park-3116883_960_720.jpg",
-    name: "Arcoin",
-    link: "https://pixabay.com/photos/park-bench-night-evening-fog-mist-3116883/",
-    size: "large",
-  },
-  {
-    id: 19,
-    imgSrc:
-      "https://cdn.pixabay.com/photo/2016/11/22/23/44/porsche-1851246_960_720.jpg",
-    name: "Pexel",
-    link: "https://pixabay.com/photos/porsche-car-brake-lights-1851246/",
+      "https://i.pinimg.com/236x/83/d1/c6/83d1c6cc1f02221f2686afd2549b08ad.jpg",
+    name: "Vladvictoria",
+    link: "https://i.pinimg.com/236x/83/d1/c6/83d1c6cc1f02221f2686afd2549b08ad.jpg",
     size: "small",
   },
   {
     id: 20,
     imgSrc:
-      "https://cdn.pixabay.com/photo/2016/06/05/20/41/girl-1438138_960_720.jpg",
-    name: "Xusenru",
-    link: "https://pixabay.com/photos/girl-umbrella-rain-park-autumn-1438138/",
+      "https://i.pinimg.com/474x/4b/d3/a3/4bd3a397182bef9242667bb1f7062606.jpg",
+    name: "Homecare",
+    link: "https://i.pinimg.com/474x/4b/d3/a3/4bd3a397182bef9242667bb1f7062606.jpg",
     size: "large",
   },
+  {
+    id: 4,
+    imgSrc:
+      "https://images.unsplash.com/photo-1442458017215-285b83f65851?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8MTN8fHxlbnwwfHx8fHw%3D&auto=format&fit=crop&w=400&q=60",
+    name: "Mammiya",
+    link: "https://pixabay.com/photos/christmas-balls-christmas-advent-6848782/",
+    size: "medium",
+  },
+
+  {
+    id: 5,
+    imgSrc:
+      "https://i.pinimg.com/236x/46/57/52/465752efcc336245765ffea5b4e55898.jpg",
+    name: "Mammiya",
+    link: "https://i.pinimg.com/236x/46/57/52/465752efcc336245765ffea5b4e55898.jpg",
+    size: "medium",
+  },
+
+  {
+    id: 50,
+    imgSrc:
+      "https://i.pinimg.com/236x/8f/b8/de/8fb8de60926879b76e0376248ddea923.jpg",
+    name: "Mammiya",
+    link: "https://i.pinimg.com/236x/8f/b8/de/8fb8de60926879b76e0376248ddea923.jpg",
+    size: "medium",
+  },
+
+  {
+    id: 46,
+    imgSrc:
+      "https://i.pinimg.com/236x/6e/91/fd/6e91fda98597904b4092cfc48c8b91a3.jpg",
+    name: "haircut",
+    link: "https://i.pinimg.com/236x/6e/91/fd/6e91fda98597904b4092cfc48c8b91a3.jpg",
+    size: "large",
+  },
+
+  {
+    id: 6,
+    imgSrc:
+      "https://i.pinimg.com/236x/59/1d/3a/591d3a95eaebc09a54035055d3a4b527.jpg",
+    name: "Mammiya",
+    link: "https://i.pinimg.com/236x/59/1d/3a/591d3a95eaebc09a54035055d3a4b527.jpg",
+    size: "small",
+  },
+  {
+    id: 7,
+    imgSrc:
+      "https://i.pinimg.com/236x/a0/c6/f5/a0c6f55f49fa57d42d78bef5814f4071.jpg",
+    name: "Mammiya",
+    link: "https://i.pinimg.com/236x/a0/c6/f5/a0c6f55f49fa57d42d78bef5814f4071.jpg",
+    size: "large",
+  },
+
+  {
+    id: 8,
+    imgSrc:
+      "https://i.pinimg.com/236x/30/6d/1d/306d1d628ec75c4cb55fd0122ecbdd37.jpg",
+    name: "Mammiya",
+    link: "https://i.pinimg.com/236x/30/6d/1d/306d1d628ec75c4cb55fd0122ecbdd37.jpg",
+    size: "small",
+  },
+  {
+    id: 9,
+    imgSrc:
+      "https://i.pinimg.com/236x/43/75/17/437517a159f408895fc070c010b2ad91.jpg",
+    name: "Mammiya",
+    link: "https://i.pinimg.com/236x/43/75/17/437517a159f408895fc070c010b2ad91.jpg",
+    size: "small",
+  },
+  {
+    id: 10,
+    imgSrc:
+      "https://i.pinimg.com/236x/03/44/fe/0344fe3fb214d2a4cc61249962e9a477.jpg",
+    name: "Mammiya",
+    link: "https://i.pinimg.com/236x/03/44/fe/0344fe3fb214d2a4cc61249962e9a477.jpg",
+    size: "large",
+  },
+
+  {
+    id: 36,
+    imgSrc:
+      "https://i.pinimg.com/236x/d4/e5/7c/d4e57cd8ceaf23017844069704a91d6e.jpg",
+    name: "haircut",
+    link: "https://i.pinimg.com/236x/d4/e5/7c/d4e57cd8ceaf23017844069704a91d6e.jpg",
+    size: "medium",
+  },
+
+  {
+    id: 11,
+    imgSrc:
+      "https://i.pinimg.com/236x/26/05/d9/2605d9f453f29081e766573bbc0ecdba.jpg",
+    name: "Mammiya",
+    link: "https://i.pinimg.com/236x/26/05/d9/2605d9f453f29081e766573bbc0ecdba.jpg",
+    size: "medium",
+  },
+  {
+    id: 12,
+    imgSrc:
+      "https://i.pinimg.com/236x/2a/02/0d/2a020d769600d3841c396097529b09b1.jpg",
+    name: "Mammiya",
+    link: "https://i.pinimg.com/236x/2a/02/0d/2a020d769600d3841c396097529b09b1.jpg",
+    size: "large",
+  },
+
+  {
+    id: 48,
+    imgSrc:
+      "https://i.pinimg.com/236x/ce/76/00/ce7600ffb7e9412c6b437a256ade44c9.jpg",
+    name: "Mammiya",
+    link: "https://i.pinimg.com/236x/ce/76/00/ce7600ffb7e9412c6b437a256ade44c9.jpg",
+    size: "large",
+  },
+
+  {
+    id: 49,
+    imgSrc:
+      "https://i.pinimg.com/236x/4b/5b/de/4b5bde544ce28a42ee9253936553638a.jpg",
+    name: "Mammiya",
+    link: "https://i.pinimg.com/236x/4b/5b/de/4b5bde544ce28a42ee9253936553638a.jpg",
+    size: "small",
+  },
+
+  {
+    id: 49,
+    imgSrc:
+      " https://i.pinimg.com/474x/fe/f1/b9/fef1b9ea56fad02e193dd597007583d4.jpg",
+    name: "Mammiya",
+    link: " https://i.pinimg.com/474x/fe/f1/b9/fef1b9ea56fad02e193dd597007583d4.jpg",
+    size: "large",
+  },
+
+  {
+    id: 13,
+    imgSrc:
+      "https://i.pinimg.com/236x/ae/ed/0f/aeed0fc8ddc6adc2de526a63c7507903.jpg",
+    name: "Zhangliams",
+    link: "https://i.pinimg.com/236x/ae/ed/0f/aeed0fc8ddc6adc2de526a63c7507903.jpg",
+    size: "large",
+  },
+  {
+    id: 14,
+    imgSrc:
+      "https://i.pinimg.com/236x/c8/44/10/c844103165c368e4eae52f5fca5268a7.jpg",
+    name: "Mshyun",
+    link: "https://i.pinimg.com/236x/c8/44/10/c844103165c368e4eae52f5fca5268a7.jpg",
+    size: "medium",
+  },
+  {
+    id: 15,
+    imgSrc:
+      "https://i.pinimg.com/236x/18/38/f8/1838f89506955aa30b97891dde1dbda6.jpg",
+    name: "Mshyun",
+    link: "https://i.pinimg.com/236x/18/38/f8/1838f89506955aa30b97891dde1dbda6.jpg",
+    size: "small",
+  },
+
+  {
+    id: 17,
+    imgSrc:
+      "https://i.pinimg.com/474x/3e/4c/f0/3e4cf0f39acdf07d5787c30b596fbc44.jpg",
+    name: "Jansheldan",
+    link: "https://i.pinimg.com/474x/3e/4c/f0/3e4cf0f39acdf07d5787c30b596fbc44.jpg",
+    size: "large",
+  },
+  {
+    id: 18,
+    imgSrc:
+      "https://i.pinimg.com/236x/f1/17/e6/f117e67af22e325434e8e8380914750d.jpg",
+    name: "Jupilu",
+    link: "https://i.pinimg.com/236x/f1/17/e6/f117e67af22e325434e8e8380914750d.jpg",
+    size: "large",
+  },
+  {
+    id: 19,
+    imgSrc:
+      "https://i.pinimg.com/236x/5c/b5/68/5cb568062d9764346af371e2a7817240.jpg",
+    name: "Stergo",
+    link: "https://i.pinimg.com/236x/5c/b5/68/5cb568062d9764346af371e2a7817240.jpg",
+    size: "large",
+  },
+
   {
     id: 21,
     imgSrc:
-      "https://cdn.pixabay.com/photo/2017/03/16/20/20/man-2150164_960_720.jpg",
-    name: "Fifalinana",
-    link: "https://pixabay.com/photos/man-waterfalls-bathe-bathing-falls-2150164/",
+      "https://i.pinimg.com/236x/92/30/36/92303628c29f6780478244b549606870.jpg",
+    name: "Homecare",
+    link: "https://i.pinimg.com/236x/92/30/36/92303628c29f6780478244b549606870.jpg",
     size: "large",
   },
+
   {
     id: 22,
     imgSrc:
-      "https://cdn.pixabay.com/photo/2016/11/29/11/12/rain-1869119_960_720.jpg",
-    name: "Pexel",
-    link: "https://pixabay.com/photos/rain-woman-brunette-portrait-1869119/",
-    size: "medium",
+      "https://i.pinimg.com/236x/9d/97/92/9d9792747d203d8745b20717d56d4b70.jpg",
+    name: "Lasrs Nissen",
+    link: "https://i.pinimg.com/236x/9d/97/92/9d9792747d203d8745b20717d56d4b70.jpg",
+    size: "small",
   },
   {
     id: 23,
     imgSrc:
-      "https://cdn.pixabay.com/photo/2016/11/19/11/11/hands-1838659_960_720.jpg",
-    name: "Pexel",
-    link: "https://pixabay.com/photos/hands-plant-soil-grow-growing-1838659/",
+      "https://i.pinimg.com/236x/64/c2/bd/64c2bd8978113fc60b2dcba0667519df.jpg",
+    name: "Jell Wllington",
+    link: "https://i.pinimg.com/236x/64/c2/bd/64c2bd8978113fc60b2dcba0667519df.jpg",
     size: "medium",
   },
   {
     id: 24,
     imgSrc:
-      "https://cdn.pixabay.com/photo/2019/01/25/11/18/girl-3954232_960_720.jpg",
-    name: "Pexel",
-    link: "https://pixabay.com/photos/girl-rain-femininity-beauty-paris-3954232/",
-    size: "large",
+      "https://i.pinimg.com/236x/eb/ae/08/ebae085c8780e3e7c5c6e1a8ab5e214f.jpg",
+    name: "Chraecker",
+    link: "https://i.pinimg.com/236x/eb/ae/08/ebae085c8780e3e7c5c6e1a8ab5e214f.jpg",
+    size: "medium",
   },
   {
     id: 25,
     imgSrc:
-      "https://cdn.pixabay.com/photo/2013/02/20/10/32/snail-83672_960_720.jpg",
-    name: "Larisa",
-    link: "https://pixabay.com/illustrations/snail-drops-rain-raindrops-mollusk-83672/",
-    size: "samll",
+      "https://i.pinimg.com/236x/e1/2d/eb/e12debe095860440c2f1b95d7f02d05f.jpg",
+    name: "Comfreak",
+    link: "https://i.pinimg.com/236x/e1/2d/eb/e12debe095860440c2f1b95d7f02d05f.jpg",
+    size: "large",
   },
   {
     id: 26,
     imgSrc:
-      "https://cdn.pixabay.com/photo/2020/08/30/09/28/buildings-5528981_960_720.jpg",
-    name: "China Town",
-    link: "https://pixabay.com/photos/buildings-houses-street-city-rain-5528981/",
+      "https://i.pinimg.com/236x/ec/90/28/ec902845d872080c7be6c248c02b9396.jpg",
+    name: "Sasinet",
+    link: "https://i.pinimg.com/236x/ec/90/28/ec902845d872080c7be6c248c02b9396.jpg",
     size: "small",
   },
   {
     id: 27,
     imgSrc:
-      "https://cdn.pixabay.com/photo/2017/03/12/02/20/triangle-2136288_960_720.jpg",
-    name: "Pyramid",
-    link: "https://pixabay.com/illustrations/triangle-sky-abstract-wallpaper-2136288/",
+      "https://i.pinimg.com/236x/f7/63/a7/f763a784b7e8fe3c744951b33d7d12aa.jpg",
+    name: "Hauad",
+    link: "https://i.pinimg.com/236x/f7/63/a7/f763a784b7e8fe3c744951b33d7d12aa.jpg",
     size: "large",
   },
   {
     id: 28,
     imgSrc:
-      "https://cdn.pixabay.com/photo/2020/04/02/23/01/rain-4996916_960_720.jpg",
-    name: "Pyou",
-    link: "https://pixabay.com/photos/rain-street-tables-cafe-flowers-4996916/",
-    size: "small",
+      "https://i.pinimg.com/236x/05/9b/2d/059b2d62cf6112d8a38c398fe26e3978.jpg",
+    name: "Chau min",
+    link: "https://i.pinimg.com/236x/05/9b/2d/059b2d62cf6112d8a38c398fe26e3978.jpg",
+    size: "medium",
   },
   {
     id: 29,
     imgSrc:
-      "https://cdn.pixabay.com/photo/2020/05/19/13/05/heart-5190672_960_720.jpg",
-    name: "Heart",
-    link: "https://pixabay.com/photos/heart-window-rain-drops-love-5190672/",
-    size: "small",
+      "https://i.pinimg.com/236x/7b/5d/22/7b5d226566d9c9d980821d0e8ff7f823.jpg",
+    name: "Steven",
+    link: "https://i.pinimg.com/236x/7b/5d/22/7b5d226566d9c9d980821d0e8ff7f823.jpg",
+    size: "medium",
   },
   {
     id: 30,
     imgSrc:
-      "https://cdn.pixabay.com/photo/2014/11/30/14/11/cat-551554_960_720.jpg",
-    name: "cat",
-    link: "https://pixabay.com/photos/cat-kitten-pet-kitty-young-cat-551554/",
+      "https://i.pinimg.com/236x/79/f7/f6/79f7f6da573aef63788da5cba0526bdf.jpg",
+    name: "Food",
+    link: "https://i.pinimg.com/236x/79/f7/f6/79f7f6da573aef63788da5cba0526bdf.jpg",
+    size: "medium",
+  },
+  {
+    id: 31,
+    imgSrc:
+      "https://i.pinimg.com/236x/22/ac/57/22ac579c4f88feb7681d7d607bf2d348.jpg",
+    name: "Arcoin",
+    link: "https://i.pinimg.com/236x/22/ac/57/22ac579c4f88feb7681d7d607bf2d348.jpg",
+    size: "small",
+  },
+  {
+    id: 32,
+    imgSrc:
+      "https://i.pinimg.com/236x/a0/c8/59/a0c8595caa955eaeba1870ec4f77126a.jpg",
+    name: "Pexel",
+    link: "https://i.pinimg.com/236x/a0/c8/59/a0c8595caa955eaeba1870ec4f77126a.jpg",
     size: "large",
+  },
+  {
+    id: 33,
+    imgSrc:
+      "https://i.pinimg.com/236x/bf/d4/01/bfd4014ff1f83cecb60524c21b22569f.jpg",
+    name: "Xusenru",
+    link: "https://i.pinimg.com/236x/bf/d4/01/bfd4014ff1f83cecb60524c21b22569f.jpg",
+    size: "medium",
+  },
+  {
+    id: 34,
+    imgSrc:
+      "https://i.pinimg.com/236x/e3/87/b3/e387b39ccd2db7de9efef5a3526eded2.jpg",
+    name: "Fifalinana",
+    link: "https://i.pinimg.com/236x/e3/87/b3/e387b39ccd2db7de9efef5a3526eded2.jpg",
+    size: "small",
+  },
+  {
+    id: 35,
+    imgSrc:
+      "https://i.pinimg.com/236x/ee/b9/26/eeb9264b9c2b46d993843e77e2eaf92b.jpg",
+    name: "Pexel",
+    link: "https://i.pinimg.com/236x/ee/b9/26/eeb9264b9c2b46d993843e77e2eaf92b.jpg",
+    size: "medium",
+  },
+
+  {
+    id: 37,
+    imgSrc:
+      "https://i.pinimg.com/236x/c5/ed/67/c5ed67347bb6ed49373a927e7b07dcbe.jpg",
+    name: "Pexel",
+    link: "https://i.pinimg.com/236x/c5/ed/67/c5ed67347bb6ed49373a927e7b07dcbe.jpg",
+    size: "small",
+  },
+  {
+    id: 38,
+    imgSrc:
+      "https://i.pinimg.com/236x/86/00/a6/8600a6976139fd2f23deb188ed017b7e.jpg",
+    name: "Pexel",
+    link: "https://i.pinimg.com/236x/86/00/a6/8600a6976139fd2f23deb188ed017b7e.jpg",
+    size: "large",
+  },
+  {
+    id: 39,
+    imgSrc:
+      "https://i.pinimg.com/236x/e4/94/4f/e4944f246b1a75492bd6af82571dada8.jpg",
+    name: "Larisa",
+    link: "https://i.pinimg.com/236x/e4/94/4f/e4944f246b1a75492bd6af82571dada8.jpg",
+    size: "large",
+  },
+  {
+    id: 40,
+    imgSrc:
+      "https://i.pinimg.com/236x/0f/be/57/0fbe57d877c96711de18d9835a938458.jpg",
+    name: "China Town",
+    link: "https://i.pinimg.com/236x/0f/be/57/0fbe57d877c96711de18d9835a938458.jpg",
+    size: "small",
+  },
+  {
+    id: 41,
+    imgSrc:
+      "https://i.pinimg.com/236x/5e/65/b9/5e65b99caf21e46e872e0838b1c8450c.jpg",
+    name: "Pyramid",
+    link: "https://i.pinimg.com/236x/5e/65/b9/5e65b99caf21e46e872e0838b1c8450c.jpg",
+    size: "large",
+  },
+  {
+    id: 42,
+    imgSrc:
+      "https://i.pinimg.com/236x/e0/77/10/e077100797ee68863ffbffcad3aefa98.jpg",
+    name: "Pyou",
+    link: "https://i.pinimg.com/236x/e0/77/10/e077100797ee68863ffbffcad3aefa98.jpg",
+    size: "small",
+  },
+  {
+    id: 43,
+    imgSrc:
+      "https://i.pinimg.com/236x/8b/bd/2a/8bbd2a4f4b24f4139de94f356734060f.jpg",
+    name: "Heart",
+    link: "https://i.pinimg.com/236x/8b/bd/2a/8bbd2a4f4b24f4139de94f356734060f.jpg",
+    size: "large",
+  },
+  {
+    id: 44,
+    imgSrc:
+      "https://i.pinimg.com/236x/1d/a6/9a/1da69ad4f7266fb7f54e62e86ef6f939.jpg",
+    name: "cat",
+    link: "https://i.pinimg.com/236x/1d/a6/9a/1da69ad4f7266fb7f54e62e86ef6f939.jpg",
+    size: "small",
+  },
+  {
+    id: 45,
+    imgSrc:
+      "https://i.pinimg.com/236x/ce/a5/86/cea586e32023f9008f000a9130bf299c.jpg",
+    name: "sunflower",
+    link: "https://i.pinimg.com/236x/ce/a5/86/cea586e32023f9008f000a9130bf299c.jpg",
+    size: "large",
+  },
+
+  {
+    id: 51,
+    imgSrc:
+      "https://i.pinimg.com/236x/fb/12/ca/fb12caa18e7b22fbf81817b2e088f15e.jpg",
+    name: "sunflower",
+    link: "https://i.pinimg.com/236x/fb/12/ca/fb12caa18e7b22fbf81817b2e088f15e.jpg",
+    size: "small",
   },
 ];
 
