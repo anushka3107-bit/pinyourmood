b9162492fa8c397ef9ba7bfe6ca2f154a5fdf56c
Nishant Chauhan <nishantpssn720@gmail.com>
2023-06-22 14:46:09 +0530

    latest commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import LandingPage from "./Components/LandingPage";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 import Home from "./Components/AfterSignIn/Home";
-import Signup from "./Components/SignUp";
 
 function App() {
   return (
@@ -10,7 +9,6 @@ function App() {
         <Routes>
           <Route path="/" element={<LandingPage />} />
           <Route path="/home" element={<Home />} />
-          <Route path="/signup" element={<Signup />} />
         </Routes>
       </BrowserRouter>
     </div>
