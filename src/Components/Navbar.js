6968b594face4bf80a030d3d0314ab00817d6925
Anushka <bhardwajanushka180@gmail.com>
2023-06-25 16:17:39 +0530

    updated hover color of navbar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,6 +1,5 @@
 import React from "react";
 const Navbar = () => {
- 
   return (
     <nav className="py-5 w-full">
       <div className="container mx-auto px-3 flex justify-between items-center">
@@ -10,32 +9,32 @@ const Navbar = () => {
             alt="Logo"
             className="cursor-pointer w-10 h-10 rounded-2xl "
           />
-          <span className="cursor-pointer text-xl font-semibold ml-2 text-black">
-            Pin Your Mood
+          <span className="cursor-pointer text-xl font-semibold ml-2 text-black hover:text-cSearchIcon">
+            PinYourMood
           </span>
         </a>
         <div className="flex items-center">
           <a
             href="/"
-            className="pr-10 font-medium text-black hover:text-cSearchText"
+            className="pr-10 font-medium text-black hover:text-cSearchIcon"
           >
             About
           </a>
           <a
             href="/"
-            className="pr-10 font-medium text-black hover:text-cSearchText"
+            className="pr-10 font-medium text-black hover:text-cSearchIcon"
           >
             Business
           </a>
           <a
             href="/"
-            className="pr-10 font-medium text-black hover:text-cSearchText"
+            className="pr-10 font-medium text-black hover:text-cSearchIcon"
           >
             Blog
           </a>
           <a
             href="/login"
-            className="bg-cSearchIcon cursor-pointer mr-4 px-3 py-2 rounded-3xl hover:bg-cHoverColorOrange"
+            className="bg-cSearchIcon cursor-pointer mr-4 px-5 py-2 rounded-3xl hover:bg-cHoverColorOrange text-white"
           >
             Login
           </a>
